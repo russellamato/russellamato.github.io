b25266d6acfa3ee2668688bbcde2f79ed6329b41
russellamato <142246787+russellamato@users.noreply.github.com>
2026-01-21 18:53:39 +0000

    completed data shapes project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -47,16 +47,37 @@ $(document).ready(function () {
     dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  
+  for (var key in dataShapes){
+      var currentObject = dataShapes[key]
+
+      if (currentObject.color === "red"){
+        currentObject.goodBehavior = 'bounce'
+      }else if(currentObject.color === "blue") {
+        currentObject.goodBehavior = 'blink'
+      }else {
+        currentObject.goodBehavior = 'spin'
+      };
+  };
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+     setBackgroundWithObject(data)
+     animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  };
 
   // TODO 5-a: add a function that handles the bad display type
+  function handleBad(data, repeat) {
+    repeat++;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3;
   
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -64,17 +85,20 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex])
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex]
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex] 
+    var repeat = currentShape.repeat
+    handleBad(currentShape, repeat);
   }
 
   /////////////////////////////////////////////////
