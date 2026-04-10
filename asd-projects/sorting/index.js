28a5a2279dd2cd5d047536dd3efd4d3e0969b46d
russellamato <142246787+russellamato@users.noreply.github.com>
2026-04-10 23:09:26 +0000

    saving sorting exercise

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,16 +14,60 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array){
+    for(var i = 0; i < array.length - 1; i++){ //outer loop, tracks the amount of sorted elements
+        for(var j = array.length - 1; j > i; j--){  // inner loop, loops through until j is greater than i and then stops
+            if (array[j].value < array[j-1].value){ // checks for if the right value of two is greater and if so it does the following code 
+                swap(array, j, j - 1) // swaps the array's positions
+                updateCounter(bubbleCounter); 
+                await sleep();
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
-
+async function quickSort(array, left, right){ // a sorting method
+    
+    if (right - left > 0){ // helps to check if it is sorted or not, not being true if it is sorted
+        index = await partition(array, left, right)
+    if (left < index - 1){ //checks for 2 elements
+        await quickSort(array, left, index - 1) // sorts the elements
+    }
+    if (index < right){ // checks for 2 elements 
+        await quickSort(array, index, right) // sorts the elements
+    }
+   }
+    
+}
 
 // TODOs 4 & 5: Implement partition
-
+async function partition(array, left, right){ // a helper function that does a lot of function for the quickSort function
+var pivot = array[Math.floor((right + left) / 2)].value;
+  while(left < right){ // while left is less than right keep running this
+    while (array[left].value < pivot){ 
+        left++; 
+    }
+    while (array[right].value > pivot){
+        right--;
+    }
+    if (left < right){ 
+        swap(array, left, right)
+        updateCounter(quickCounter)
+        await sleep();
+    }      
+    
+  }
+    return left + 1
+}
 
 // TODO 1: Implement swap
-
+function swap (array, i, j){ // swaps two indexes of an array, takes the array and the two indexes as arguments
+    var temp = array[i] // makes a temporary value to hold the first position
+    array[i] = array[j] // overides the position of i in the array with the position of of j in the array
+    array[j] = temp // overides the position of j in the array with the value of temp allowing for it to be given the inital position of i in the array
+    drawSwap(array, i, j) // makes the animation happen when swapping
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
